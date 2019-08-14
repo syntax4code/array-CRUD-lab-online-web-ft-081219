4bae152ae38f49cf75679a1c4dd74f648e2c07c2
Warren Loy Harper II <warrenharper2@att.net>
2019-08-14 21:49:53 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,32 +1,39 @@
 def create_an_empty_array
-  
+  empty_array = []
 end
 
 def create_an_array
-  
+  array_with_four_elements = ["dogs", "cats", "horses", "wow"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array_with_four_elements = ["dogs", "cats", "horses", "arrays!"]
+  array_with_four_elements << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array_with_four_elements = ["dogs", "cats", "horses", "arrays!]
+  array_with_four_elements.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array_with_four_elements = ["dogs", "cats", "horses", "arrays!"]
+  reptile_pets = array_with_four_elements.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array_with_four_elements = ["dogs", "cats", "horses", "arrays!"]
+  canines_domestic = array_with_four_elements.shift 
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array_with_four_elements = ["dogs", "cats", "horses", "arrays!"]
+  array_with_four_elements[0]
 end
 
 def retrieve_first_element_from_array(array)
+  array_with_four_elements = ["dogs", "cats", "horses", "arrays!"]
+  array_with_four_elements.first
   
 end
 
