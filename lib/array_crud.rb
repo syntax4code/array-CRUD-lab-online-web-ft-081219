3a9e4f2d9a8a8b2d341ba24bdca8cd8672ab3840
Warren Loy Harper II <warrenharper2@att.net>
2019-08-14 21:47:38 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,7 @@ end
 
 def add_element_to_start_of_array(array, element)
   array_with_four_elements = ["dogs", "cats", "horses", "snakes"]
-  array_with_four_elements.unshift("cows")
+  array_with_four_elements.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
